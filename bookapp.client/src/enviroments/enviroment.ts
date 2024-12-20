b16aca2f490625a2760f29fdb6d41384fdfb24e7
Ivy Analisa <ivy.analisa@gmail.com>
2024-12-20 23:12:51 +0100

    Change enviroment files

--- bookapp.client/src/enviroments/enviroment.ts
+++ bookapp.client/src/enviroments/enviroment.ts
@@ -1,6 +1,6 @@
 // src/environments/environment.ts
 export const environment = {
     production: false,  // Set production to false for development
-    apiUrl: 'https://localhost:7149/api',  // API URL for your local development server
+    apiUrl: 'https://book-web-api-bbemd6c3hghyg4a7.northeurope-01.azurewebsites.net/api',  // API URL for your local development server
   };
   
